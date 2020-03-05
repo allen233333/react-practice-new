8fdf0b3fc6f8f4a17fe5c4d4e957ca72023080ba
allen233333 <60804452+allen233333@users.noreply.github.com>
2020-03-05 00:49:25 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Course {
+    constructor(college, department, number, section, professor) {
+        this.college = college;
+        this.department = department;
+        this.number = number;
+        this.section = section;
+        this.professor = professor;
+        this.key = `${college} ${department} ${number} ${section}`;
+    }
+}
+
+class App extends Component {
+    render() {
+        const columns = [
+            'College',
+            'Department',
+            'Number',
+            'Section',
+            'Professor',
+        ];
+        const courses = [
+            new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
+            new Course('CAS', 'CS', '332', 'A1', 'Bun'),
+            new Course('CAS', 'CS', '350', 'A1', 'Sarkar'),
+        ];
+
+        return (
+            <div className="App">
+                <table>
+                    <thead>
+                    <tr>
+                        {columns.map(column =>
+                            <th key={column}>{column}</th>)}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        courses.map(course =>
+                            <tr key={course.key}>
+                                <td>{course.college}</td>
+                                <td>{course.department}</td>
+                                <td>{course.number}</td>
+                                <td>{course.section}</td>
+                                <td>{course.professor}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default App;
